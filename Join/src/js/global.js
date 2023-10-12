9f3d54a510829775db98c4d8a15949a800933b3c
Leo Albert <leo.albert.96@web.de>
2023-10-12 16:48:48 +0200

    finished project already

---
+++ Join/src/js/global.js
@@ -0,0 +1,95 @@
+function includeHTML() {
+    let z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+    getActiveLink();
+}
+
+function getActiveLink() {
+    if (window.location.href.indexOf("summary") > -1) {
+        addActiveBgr('summary_link', 'mobile_summary_link');
+    }
+    if (window.location.href.indexOf("board") > -1) {
+        addActiveBgr('board_link', 'mobile_board_link');
+    }
+    if (window.location.href.indexOf("add-task") > -1) {
+        addActiveBgr('add_task_link', 'mobile_add_task_link');
+    }
+    if (window.location.href.indexOf("contacts") > -1) {
+        addActiveBgr('contact_link', 'mobile_contact_link');
+    }
+    if (window.location.href.indexOf("legal-notice") > -1) {
+        let element = document.getElementById("legal_notice_link");
+        element.classList.add("active");
+    }
+}
+
+window.addEventListener('resize', () => {
+    getActiveLink();
+});
+
+function addActiveBgr(id1, id2) {
+    let element = document.getElementById(id1);
+    let element2 = document.getElementById(id2);
+    if (window.innerWidth <= 468) {
+        element2.classList.add("active");
+    } else {
+        element.classList.add("active");
+    }
+}
+
+function openHeaderMenu() {
+    if (window.innerWidth >= 468) {
+        document.getElementById('header_menu').classList.remove('d-none');
+    } else {
+        document.getElementById('mobile_header_menu').classList.remove('d-none');
+    }
+    
+}
+
+function closeHeaderMenu() {
+    if (window.innerWidth >= 468) {
+        document.getElementById('header_menu').classList.add('d-none');
+    } else {
+        document.getElementById('mobile_header_menu').classList.add('d-none');
+    }
+}
+
+function toggleHelp() {
+    document.getElementById('help_link').classList.toggle('d-none');
+    document.getElementById('help_includer').classList.toggle('d-none');
+}
+
+function stopPropagation(event) {
+    event.stopPropagation();
+}
+
+async function logout() {
+    await resetLoginStatus();
+    document.location.href = 'login.html';
+}
+
+async function resetLoginStatus() {
+    for (let i = 0; i < accounts.length; i++) {
+        accounts[i].online = false;
+        accounts[i].greeting = 0;
+    }
+    await saveAccount();
+}
